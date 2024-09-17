3d05c828a9811dbb684b9195ef760784f9c37ed4
Sima Bilony <www.simabilony@gmail.com>
2024-09-17 20:46:33 +0300

    FixBug

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,22 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    /**
+    * @OA\Info(
+    *     version="3.0",
+    *     title="API Documentation",
+    * )
+    *    @OA\PathItem(path="/api")
+    *   @OA\Server(url="http://eavtask.test")
+    * @OA\SecurityScheme(
+    *   securityScheme="bearerAuth",
+    *   type="http",
+    * scheme="bearer"
+    *)
+    */
 }
--- app/Http/Controllers/EntityController.php
+++ app/Http/Controllers/EntityController.php
@@ -5,6 +5,7 @@ use App\Http\Requests\StoreEntityRequest;
 use App\Models\Entity;
 use App\Services\EntityService;
 use Illuminate\Http\Request;
+use OpenApi\Annotations as OA;
 
 class EntityController extends Controller
 {
@@ -13,6 +14,37 @@ class EntityController extends Controller
     {
         $this->entityService = $entityService;
     }
+/**
+* @OA\PathItem(path="/api")
+* @OA\Get(
+*     path="/api/products/{slug}",
+*     tags = {"Products"},
+*     summary="Get one product",
+   *     @OA\Parameter(
+   *      name="Accept-Language",
+   *      in="header",
+   *     required=true, schema={"type": "string", "enum": {"en", "ar" , "es" , "fr", "fa" , "zh" , "ru"}, "default": "en"}
+   *     ),
+   *     @OA\Parameter(
+   *         in="path",
+   *         name="slug",
+   *         required=true,
+   *         @OA\Schema(type="string"),
+   *     ),
+     *     @OA\Parameter(
+     *          name="convertCurrency",
+     *          in="query",
+     *          required=false,
+     *          description="Currency to convert product price",
+     *          schema={"type": "string", "enum": {"KWD", "SYP", "USD", "SAR", "EUR"}, "default": "KWD"}
+     *     ),
+   *     @OA\Response(
+   *         response=200,
+   *         description="OK"
+    *     )
+   * )
+   * /
+   **/
     public function store(StoreEntityRequest $request): \Illuminate\Http\JsonResponse
     {
         $entity = $this->entityService->createEntity($request->validated());
