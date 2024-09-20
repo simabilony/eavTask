0192a77b7655fdb6d54cc3abd7462ab80fd3521f
Sima Bilony <www.simabilony@gmail.com>
2024-09-20 13:23:41 +0300

    fixBug

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,13 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use OpenApi\Annotations as OA;
-
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="E-Shop Catalog",
+ *     description="API Documentation",
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
